f79dca1cdf60e860dd3a155dcf0a93214365acab
Tanvir Ahmed <tanvir007v@gmail.com>
2023-09-08 21:44:39 -0500

    React frontend setup for the project

---
+++ frontend-code/src/components/Public.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+const Public = () => {
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Welcome to <span className="nowrap">Lucas M. Repairs!</span>
+        </h1>
+      </header>
+      <main className="public__main">
+        <p>
+          Located in Beautiful Downtown Fargo City, Lucas M. Repairs provides a
+          trained staff ready to meet your tech repair needs.
+        </p>
+        <address className="public__addr">
+          Lucas M. Repairs
+          <br />
+          Main Ave Drive
+          <br />
+          Fargo City, ND 58103
+          <br />
+          <a href="tel:+15555555555">(555) 555-5555</a>
+        </address>
+        <br />
+        <p>Owner: Lucas Morrison</p>
+      </main>
+      <footer>
+        <Link to="/login">Employee Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+
+export default Public;
